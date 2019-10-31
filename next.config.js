fb98882e85fd11102f6190fefa3a75b2daedb3b7
benhid <antonio.b@uma.es>
2019-10-31 08:52:36 +0000

    Update  version

--- next.config.js
+++ next.config.js
@@ -1,15 +1,3 @@
 const withCSS = require("@zeit/next-css");
 
 module.exports = withCSS({});
-module.exports = {
-    exportPathMap: function() {
-      return {
-        '/': { page: '/' },
-        '/about': { page: '/about' },
-        '/p/hello-nextjs': { page: '/post', query: { title: 'Hello Next.js' } },
-        '/p/learn-nextjs': { page: '/post', query: { title: 'Learn Next.js is awesome' } },
-        '/p/deploy-nextjs': { page: '/post', query: { title: 'Deploy apps with Zeit' } }
-      }
-    }
-  }
-  
